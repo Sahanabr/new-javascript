3dbf48a77207d78ca8e1ea8c3fd3b93bab72a721
Sahana B R <sahanabr843@gmail.com>
2025-11-24 09:59:15 +0530

    commit

--- arrowFunction.js
+++ arrowFunction.js
@@ -27,5 +27,27 @@ const sum=(a,b) => a+b;
 // let value =sum(5,10);
 // console.log(value);
 
-console.log(+true);
+// console.log(+true);
 
+setTimeout(()=>{
+    console.log("value one");
+},3000);
+
+setTimeout(()=>{
+    console.log("value two");
+},2000);
+
+setTimeout(()=>{
+    console.log("value three");
+},1000);
+
+//It is called as asynchronous behavior in javascript.
+
+
+console.log("value one");
+console.log("value two");
+console.log("value three");
+
+//It is called as synchronous behavior (happens sequentially) in javascript.
+
+//in real time scenario we use asynchronous behavior.
